b1a0a6d0dc3695c56ed0c87cc073e0c01607621b
serge-sans-paille <sguelton@redhat.com>
2021-03-02 14:55:51 +0100

    Make python-embedded-interpreter/runtest.sh more debuggable

--- tests/python-embedded-interpreter/runtest.sh
+++ tests/python-embedded-interpreter/runtest.sh
@@ -4,8 +4,10 @@ set -ex
 
 g++ -g test.cpp
 
-test `lldb -b -o 'breakpoint set --file test.cpp --line 7' -o run -o 'p v' -- a.out \
-  | grep \
+lldb -b -o 'breakpoint set --file test.cpp --line 7' -o run -o 'p v' -- a.out | tee lldb.log
+
+test `grep \
     -e '(std::vector<int, std::allocator<int> >) $0 = size=1 {' \
     -e '\[0\] = 2' \
+    lldb.log \
   | wc -l` -eq 2
